6b6f58e88bed78e3ec0ba3a9b13a7bba813d0fed
Nathaniel Lee <leenathaniel001@gmail.com>
2025-03-17 09:42:47 -0400

    Update Indicators.py

--- Indicators.py
+++ Indicators.py
@@ -15,4 +15,21 @@ def simple_moving_average(ticker,period):
     PMA = PMA.iat[0]
 
     return form_b(PMA)
+    
+def equity_corr(ticker1,ticker2,period):
+    import Data_Funcs as df
+    import pandas as pd
+    import numpy as np
 
+    pd.set_option('display.max_rows', None)  
+    pd.set_option('display.max_columns', None)
+
+    equityone = df.equity_data(ticker1,period)
+    equitytwo = df.equity_data(ticker2,period)
+    equityone = equityone["Close"]
+    equitytwo = equitytwo["Close"]
+
+    combined_df = equityone.join(equitytwo, lsuffix = ticker1,rsuffix = ticker2)
+    corr_matrix = combined_df.corr()
+
+    return corr_matrix
