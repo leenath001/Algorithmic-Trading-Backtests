ae6fc5bf1017bceb3ce8c73633efd951256af534
Nathaniel Lee <leenathaniel001@gmail.com>
2025-04-01 09:51:57 -0400

    Update Backtst.py
    Updating with column that tracks actions, plotting update

--- Backtst.py
+++ Backtst.py
@@ -19,15 +19,14 @@ def is_pos(n):
 def SMA_backtest(ticker,SMA_window): 
 
     # Backtest for Simple Moving Average Strategy. SMA_window gives period for rolling average to be calculated 
-    # Buy conditions: Buy first instance of SMA < equity price. Hold for all other instances following.
-    # Sell conditions: Sell first instance of SMA > equity price. Do nothing for all other instances following. 
+    # Buy conditions: Buy first instance of SMA > equity price. Hold for all other instances following.
+    # Sell conditions: Sell first instance of SMA < equity price. Do nothing for all other instances following. 
     
     # SMA period
     window = SMA_window
-    ticker = 'AAPL'
 
     # getting equity data, SMA data, and Boolean data (used to define when entry threshold crossed)
-    data = yf.download(ticker, period='3y', interval='1d')
+    data = yf.download(ticker, period='1y', interval='1d')
     SMA = data['Close'].rolling(window).mean()
     tst = len(SMA) - (window)
     equity = data['Close']
@@ -36,13 +35,14 @@ def SMA_backtest(ticker,SMA_window):
     delta = delta.apply(is_pos)
     delta = pd.DataFrame(delta)
 
-    # True -> SMA < equity, buy/hold. False -> equity > SMA, do nothing/sell
+    # True -> SMA > equity, buy/hold. False -> equity > SMA, do nothing/sell
 
     # Running backtest
     P = 0 # Boolean. 0 -> no position, 1 -> long positon
     alo = 100 # initial allocation
     valuevec = alo * np.ones(len(equity)) # stores bt historical values
     buyhold = alo * np.ones(len(equity)) # values of buy/hold 
+    actionvec = np.empty(len(equity),object)
 
     # condition to run 
     for i in range(1,len(equity)):
@@ -53,31 +53,43 @@ def SMA_backtest(ticker,SMA_window):
         if P == 0 and delta1 == False and delta2 == True: #buy
             P = 1
             valuevec[i] = valuevec[i-1] * (1 +  (equity.iloc[i,0] - equity.iloc[i-1,0])/equity.iloc[i-1,0])
+            actionvec[i] = 'B'
 
         elif P == 1 and delta1 == False and delta2 == False: # hold
             valuevec[i] = valuevec[i-1] * (1 +  (equity.iloc[i,0] - equity.iloc[i-1,0])/equity.iloc[i-1,0])
-    
-        elif P == 1 and delta1 == True and delta2 == False or P == 0 and delta1 == True and delta2 == True: # sell/do nothing
+            actionvec[i] = 'H'
+
+        elif P == 1 and delta1 == True and delta2 == False: # sell
             P = 0 
             valuevec[i] = valuevec[i-1]
+            actionvec[i] = 'S'
+
+        elif P == 0 and delta1 == True and delta2 == True: # do nothing
+            valuevec[i] = valuevec[i-1]
+            actionvec[i] = 'NA'
 
     # creating dataframe for comparison of strategy
     buyhold = pd.DataFrame(buyhold, index = equity.index)
-    buyhold = buyhold.rename(columns={buyhold.columns[0]: 'BT Vals'})
+    buyhold = buyhold.rename(columns={buyhold.columns[0]: 'Buy/Hold'})
     valuevec = pd.DataFrame(valuevec, index = equity.index)
-    valuevec = valuevec.rename(columns={valuevec.columns[0]: 'BT Vals'})
+    actionvec = pd.DataFrame(actionvec, index = equity.index)
+    actionvec = actionvec.rename(columns={actionvec.columns[0]: 'Action'})
+    valuevec = valuevec.rename(columns={valuevec.columns[0]: 'Strat Val'})
     equity = equity.rename(columns={equity.columns[0]: 'Close Prices'})
-    SMA = SMA.rename(columns={SMA.columns[0]: '5d SMA'})
+    SMA = SMA.rename(columns={SMA.columns[0]: '{}d SMA'.format(window)})
     delta = delta.rename(columns={delta.columns[0]: 'isPos'})
-    comb = pd.concat([equity, SMA, delta,valuevec,buyhold], axis=1)
+    comb = pd.concat([equity, SMA, delta,actionvec, valuevec,buyhold], axis=1)
     print()
-    print(comb.iloc[window-1:,:])
+    print(comb.iloc[window-1:,:])  
 
     plt.figure()
-    plt.plot(comb.index,valuevec)
-    plt.plot(comb.index,buyhold)
+    plt.plot(comb.index,valuevec,label = 'Strategy')
+    plt.plot(comb.index,buyhold,label = 'Buy/Hold')
+    plt.plot(comb.index,comb.loc[:,'Close Prices'],label = '{} Close Prices'.format(ticker))
+    plt.plot(comb.index,comb.loc[:,'5d SMA'],label = '{}d SMA'.format(window))
     plt.xlabel("Timestamp")
     plt.ylabel("Value")
-    plt.xticks(rotation=45)
-    plt.title("SMA strategy vs Buy & Hold")
+    plt.xticks(rotation=30)
+    plt.legend()
+    plt.title("SMA strategy vs Buy & Hold : {}".format(ticker))
     plt.show()
